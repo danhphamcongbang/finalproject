4fe26fb29590c5fd9e7c7dcc55024d2bda40ed93
danhphamcongbang <bang.danh_dp04@devplus.edu.vn>
2022-12-06 10:40:12 +0700

    done task API div 1

--- src/components/task__bang/task1/FE02Typical.js
+++ src/components/task__bang/task1/FE02Typical.js
@@ -1,70 +1,79 @@
 import './FE02Typical.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getAllDataFE02} from"../../../service/Apiservice"
 import {FaFacebookSquare, FaLinkedin} from "react-icons/fa";
-import {Link} from "react-router-dom";
 import FE02TypicalChild from "./FE02TypicalChild";
+import axios from "axios";
 
 const FE02Typical = () => {
-    const [list, setList] = useState([
-        {
-            id: "fe02-1",
-            image: "https://stunited.vn/wp-content/uploads/2019/09/Capture.png"
-            ,
-            teamCaption: "eHealth",
-            teamDesigner: "#ASP.NET #Android #iOS",
-            teamCaptionHide: "A health data management application for " +
-                "the personal healthcare in coordination with employers, pharmacies and medical examination providers."
-        },
-        {
-            id: "fe02-2", image: "https://stunited.vn/wp-content/uploads/2019/09/image-logo-1.png"
-            ,teamCaption: "Havuta", teamDesigner: "#Blockchain #NodeJS #ReactJS #React Native",
-            teamCaptionHide: "\n" +
-                "                            Havuta brings smartphone and blockchain-based solutions to the data collection challenges facing the development sector. Creating a blockchain-based feedback loop benefiting NGOs, engaging their beneficiaries, and promoting transparency. "
-        },
-        {
-            id: "fe02-3", image: "https://stunited.vn/wp-content/uploads/2019/09/figure01.jpg"
-            , teamCaption: "E-ASPRO", teamDesigner: "#Javascript #HTML #CSS",
-            teamCaptionHide: "E-ASPRO is a cloudtype fulfillment mail order solution that enables you to efficiently manage mail order operations such as site management," +
-                " orders, shipping, inventory, and shipping inquiries from one system"
-        },
-        {
-            id: "fe02-4",
-            image: "https://stunited.vn/wp-content/uploads/2019/09/2e-jycg6_400x400.jpg"
-            ,
-            teamCaption: "Vision Express",
-            teamDesigner: "#Chatbot #Javascript #HTML #CSS",
-            teamCaptionHide: "Chatbot to communicate with users, reply questions automatically."
-        },
-    ])
-    // const [titleList, setTileList] = useState([
-    //     { id: "fe-02_Title-1",title:"Typical successful stories",titleSection:"", },
-    //     { id: "fe-02_Title-2",title:"Typical successful stories",titleSection:"", },
-    //
+    // const [list, setList] = useState([
+    //     {
+    //         id: "fe02-1",
+    //         image: "https://stunited.vn/wp-content/uploads/2019/09/Capture.png"
+    //         ,
+    //         teamCaption: "eHealth",
+    //         teamDesigner: "#ASP.NET #Android #iOS",
+    //         teamCaptionHide: "A health data management application for " +
+    //             "the personal healthcare in coordination with employers, pharmacies and medical examination providers."
+    //     },
+    //     {
+    //         id: "fe02-2", image: "https://stunited.vn/wp-content/uploads/2019/09/image-logo-1.png"
+    //         , teamCaption: "Havuta", teamDesigner: "#Blockchain #NodeJS #ReactJS #React Native",
+    //         teamCaptionHide: "\n" +
+    //             "                            Havuta brings smartphone and blockchain-based solutions to the data collection challenges facing the development sector. Creating a blockchain-based feedback loop benefiting NGOs, engaging their beneficiaries, and promoting transparency. "
+    //     },
+    //     {
+    //         id: "fe02-3", image: "https://stunited.vn/wp-content/uploads/2019/09/figure01.jpg"
+    //         , teamCaption: "E-ASPRO", teamDesigner: "#Javascript #HTML #CSS",
+    //         teamCaptionHide: "E-ASPRO is a cloudtype fulfillment mail order solution that enables you to efficiently manage mail order operations such as site management," +
+    //             " orders, shipping, inventory, and shipping inquiries from one system"
+    //     },
+    //     {
+    //         id: "fe02-4",
+    //         image: "https://stunited.vn/wp-content/uploads/2019/09/2e-jycg6_400x400.jpg"
+    //         ,
+    //         teamCaption: "Vision Express",
+    //         teamDesigner: "#Chatbot #Javascript #HTML #CSS",
+    //         teamCaptionHide: "Chatbot to communicate with users, reply questions automatically."
+    //     },
     // ])
-    return (
-        <>
-            <div className="fe02-container">
-                <div className="fe02-container__wpb">
-                    <div className="fe02-container__wpb__inner">
-                        <div className="fe02-wpb__wrapper">
-                            <div className="fe02-textcenter">
-                                <h2>
-                                    Typical successful stories
-                                </h2>
-                                <h3>
-                                    From ideas turned into carefully crafted pixels and problems crafted into solutions.
-                                </h3>
-                            </div>
-                            <div className="fe02-emptyspace"></div>
-                            <FE02TypicalChild
-                                listContainerBang={list}
-                                SetListContainerBAng={setList}
-                            />
+    const [apiList, setApiList] = useState([]);
+    // useEffect = async () => {
+    //     let res = await axios.get(`http://localhost:8000/fe02-typical`);
+    //     setapiList({apilist: res && res.data && res.data.data ? res.data.data : []})
+    // }
+
+    useEffect ( () =>{
+        fetchListFE02();
+    },[])
+    const fetchListFE02 = async () =>{
+        let res =await getAllDataFE02();
+        setApiList(res);
+    }
+return (
+    <>
+        <div className="fe02-container">
+            <div className="fe02-container__wpb">
+                <div className="fe02-container__wpb__inner">
+                    <div className="fe02-wpb__wrapper">
+                        <div className="fe02-textcenter">
+                            <h2>
+                                Typical successful stories
+                            </h2>
+                            <h3>
+                                From ideas turned into carefully crafted pixels and problems crafted into solutions.
+                            </h3>
                         </div>
+                        <div className="fe02-emptyspace"></div>
+                        <FE02TypicalChild
+                            listContainerBang={apiList}
+                            SetListContainerBAng={setApiList}
+                        />
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>
+)
 }
 export default FE02Typical
--- src/components/task__bang/task2/Fe07_stsoftware.js
+++ src/components/task__bang/task2/Fe07_stsoftware.js
@@ -1,64 +1,66 @@
 import './Fe07_stsoftware.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {BsFillFlagFill} from "react-icons/bs";
 import {FaUsers} from "react-icons/fa";
 import FE02TypicalChild from "../task1/FE02TypicalChild";
+import {getAllDataFE07Div1} from "../../../service/Apiservice";
 
 const Fe07_stsoftware = () => {
-    const [listFe07, setListFe07] = useState([
-        {
-            id: "fe07-1",
-            image: "https://stunited.vn/wp-content/uploads/2019/09/Capture.png"
-            ,
-            teamCaption: "eHealth",
-            teamDesigner: "#ASP.NET #Android #iOS",
-            teamCaptionHide: "A health data management application for " +
-                "the personal healthcare in coordination with employers, pharmacies and medical examination providers."
-        },
-        {
-            id: "fe07-2", image: "https://stunited.vn/wp-content/uploads/2019/09/image-logo-1.png"
-            , teamCaption: "Havuta", teamDesigner: "#Blockchain #NodeJS #ReactJS #React Native",
-            teamCaptionHide: "\n" +
-                "                            Havuta brings smartphone and blockchain-based solutions to the data collection challenges facing the development sector. Creating a blockchain-based feedback loop benefiting NGOs, engaging their beneficiaries, and promoting transparency. "
-        },
-        {
-            id: "fe07-3", image: "https://stunited.vn/wp-content/uploads/2019/09/figure01.jpg"
-            , teamCaption: "E-ASPRO", teamDesigner: "#Javascript #HTML #CSS",
-            teamCaptionHide: "E-ASPRO is a cloudtype fulfillment mail order solution that enables you to efficiently manage mail order operations such as site management," +
-                " orders, shipping, inventory, and shipping inquiries from one system"
-        },
-        {
-            id: "fe07-4",
-            image: "https://stunited.vn/wp-content/uploads/2019/09/2e-jycg6_400x400.jpg"
-            ,
-            teamCaption: "Vision Express",
-            teamDesigner: "#Chatbot #Javascript #HTML #CSS",
-            teamCaptionHide: "Chatbot to communicate with users, reply questions automatically."
-        },
-
-    ])
-    const [listContextFe07D1, setListContextFe07D1] = useState([
-        {
-            id: "fe07-1", icon: <BsFillFlagFill/>, title: "Web App Development",
-            content: "We create beautifully designed web apps precisely tailored t" +
-                "o your situationWe deliver web systems of any size and complexity — from budding startu" +
-                "p products to hi-end enterprise solutions.",
-            link: "see more"
-        },
-        {
-            id: "fe07-2", icon: <BsFillFlagFill/>, title: "Web App Development",
-            content: "ST provides custom mobile application development across different\n" +
-                "platforms such as iOS and Android for consumer-facing and enterprise\n" +
-                "environment.",
-            link: "see more"
-        },
-        {
-            id: "fe07-3", icon: <BsFillFlagFill/>, title: "Custom Software Development",
-            content: "T provides high quality, cost-effective and reliable software development services match your specific needs, budget and timeframe." +
-                " We love to competes with other firms on the grounds of quality, creativity, n’ dedication",
-            link: "see more"
-        }
-    ])
+    // const [listFe07, setListFe07] = useState([
+    //     {
+    //         id: "fe07-1",
+    //         image: "https://stunited.vn/wp-content/uploads/2019/09/Capture.png"
+    //         ,
+    //         teamCaption: "eHealth",
+    //         teamDesigner: "#ASP.NET #Android #iOS",
+    //         teamCaptionHide: "A health data management application for " +
+    //             "the personal healthcare in coordination with employers, pharmacies and medical examination providers."
+    //     },
+    //     {
+    //         id: "fe07-2", image: "https://stunited.vn/wp-content/uploads/2019/09/image-logo-1.png"
+    //         , teamCaption: "Havuta", teamDesigner: "#Blockchain #NodeJS #ReactJS #React Native",
+    //         teamCaptionHide: "\n" +
+    //             "                            Havuta brings smartphone and blockchain-based solutions to the data collection challenges facing the development sector. Creating a blockchain-based feedback loop benefiting NGOs, engaging their beneficiaries, and promoting transparency. "
+    //     },
+    //     {
+    //         id: "fe07-3", image: "https://stunited.vn/wp-content/uploads/2019/09/figure01.jpg"
+    //         , teamCaption: "E-ASPRO", teamDesigner: "#Javascript #HTML #CSS",
+    //         teamCaptionHide: "E-ASPRO is a cloudtype fulfillment mail order solution that enables you to efficiently manage mail order operations such as site management," +
+    //             " orders, shipping, inventory, and shipping inquiries from one system"
+    //     },
+    //     {
+    //         id: "fe07-4",
+    //         image: "https://stunited.vn/wp-content/uploads/2019/09/2e-jycg6_400x400.jpg"
+    //         ,
+    //         teamCaption: "Vision Express",
+    //         teamDesigner: "#Chatbot #Javascript #HTML #CSS",
+    //         teamCaptionHide: "Chatbot to communicate with users, reply questions automatically."
+    //     },
+    //
+    // ])
+    const [listFe07, setListFe07] = useState([])
+    // const [listContextFe07D1, setListContextFe07D1] = useState([
+    //     {
+    //         id: "fe07-1", icon: <BsFillFlagFill/>, title: "Web App Development",
+    //         content: "We create beautifully designed web apps precisely tailored t" +
+    //             "o your situationWe deliver web systems of any size and complexity — from budding startu" +
+    //             "p products to hi-end enterprise solutions.",
+    //         link: "see more"
+    //     },
+    //     {
+    //         id: "fe07-2", icon: <BsFillFlagFill/>, title: "Web App Development",
+    //         content: "ST provides custom mobile application development across different\n" +
+    //             "platforms such as iOS and Android for consumer-facing and enterprise\n" +
+    //             "environment.",
+    //         link: "see more"
+    //     },
+    //     {
+    //         id: "fe07-3", icon: <BsFillFlagFill/>, title: "Custom Software Development",
+    //         content: "T provides high quality, cost-effective and reliable software development services match your specific needs, budget and timeframe." +
+    //             " We love to competes with other firms on the grounds of quality, creativity, n’ dedication",
+    //         link: "see more"
+    //     }
+    // ])
     const [listContextFe07D2, setListContextFe07D2] = useState([
         {
             id: "fe07-4", icon: <FaUsers/>, title: "Web App Development",
@@ -81,6 +83,14 @@ const Fe07_stsoftware = () => {
         }
 
     ])
+    const [listContextFe07D1, setListContextFe07D1] = useState([])
+    useEffect ( () =>{
+        fetchListFE07Div1();
+    },[])
+    const fetchListFE07Div1 = async () =>{
+        let res =await getAllDataFE07Div1();
+        setListContextFe07D1(res);
+    }
     return (
         <>
             <div className="fe07-container">
